6571569e6138be8f2e65352f4d363e30f471b390
marek pihel <marek.pihel@ut.ee>
2025-04-18 07:41:09 +0300

    Final result

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import math
 
+"""
+Juno boresight taken from https://space.stackexchange.com/questions/26457/juno-rf-power-output
+"""
+
 rf_sig_at_reciever_LNA = -165 #dBm
 antenna_efficiency = 0.6
 distance = 850949987 #km
 frequency = 8.46 #GHz
+juno_boresight_gain = 44.1 #dBi
 
-fspl = 20*math.log(distance) + 20*math.log(frequency) + 92.45
+fspl = 20*math.log10(distance) + 20*math.log10(frequency) + 92.45
+
+gain_at_antenna = fspl - juno_boresight_gain + rf_sig_at_reciever_LNA
+
+
+def calculate_antenna_diameter(G_dB, efficiency, frequency_ghz):
+    # Speed of light factor for GHz: lambda = 0.3 / f(GHz)
+    wavelength = 0.3 / frequency_ghz
+
+    # Convert gain from dB to linear scale
+    G_linear = 10 ** (G_dB / 10)
+
+    # Apply the formula
+    D = (wavelength / math.pi) * math.sqrt(G_linear / efficiency)
+
+    return D
 
 
 def print_stuff():
     print("Free space path loss: ", fspl, " dBm")
+    print("Antenna gain needed: ", gain_at_antenna, " dBi")
+    print("Dish size: ", calculate_antenna_diameter(gain_at_antenna, antenna_efficiency, frequency), " m")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_stuff()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
